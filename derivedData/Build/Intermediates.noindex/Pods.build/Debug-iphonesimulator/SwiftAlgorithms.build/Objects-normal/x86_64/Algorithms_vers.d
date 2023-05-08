21cf5af9e9bea70f9245831bfadfb49c1f5efac0
Ricardo Maqueda <rinconestolmo@gmail.com>
2023-05-08 09:53:51 +0200

    Delete build and derivedData

--- derivedData/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/SwiftAlgorithms.build/Objects-normal/x86_64/Algorithms_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/rmaqueda/Repos/Layout_UIKIT_SwiftUI_Preview/derivedData/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/SwiftAlgorithms.build/DerivedSources/Algorithms_vers.c
